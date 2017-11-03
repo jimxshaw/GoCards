94f58037aca498183996aee3c5add56a50404b91
Jim Shaw <jimxshaw@gmail.com>
2017-11-02 22:11:34 -0500

    Implement newDeck function. Call newDeck in main.go.

--- deck.go
+++ deck.go
@@ -6,6 +6,24 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+
+	// For unused variables like the i and j iterators in the
+	// loops, replace them with the _ underscore to prevent
+	// linting errors.
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- main.go
+++ main.go
@@ -3,17 +3,7 @@ package main
 func main() {
 	// Initialize a new deck type, which is
 	// a slice of strings.
-	cards := deck{"Two of Hearts", newCard()}
-
-	// Append a card to our cards variable and then
-	// re-assign it back to our cards variable.
-	// The append function does not mutate the
-	// origin slice but will return a new slice.
-	cards = append(cards, "Jack of Spades")
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Seven of Clubs"
 }
