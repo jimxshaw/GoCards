424f328600fb27ce08bc4180c5708ba2a3a0b838
Jim Shaw <jimxshaw@gmail.com>
2017-11-02 20:39:52 -0500

    Add newCard function.

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	//var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	card = "Eight of Hearts"
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Seven of Clubs"
 }
