c5d52188d7ccb2c8e04fd966117e16519052244d
Jim Shaw <jimxshaw@gmail.com>
2017-11-12 21:31:25 -0600

    Add more test scenarios in deck_test.go.

--- deck_test.go
+++ deck_test.go
@@ -13,4 +13,13 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected deck length of %v but got %v", totalCards, len(d))
 	}
 
+	// Make sure the first card in the deck is
+	// what we want it to be.
+	if d[0] != "A of Spades" {
+		t.Errorf("Expected first card to be A of Spades but instead got %v", d[0])
+	}
+
+	if d[len(d)-1] != "K of Clubs" {
+		t.Errorf("Expected last card to be K of Clubs but instead got %v", d[len(d)-1])
+	}
 }
